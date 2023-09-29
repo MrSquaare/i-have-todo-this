48aee29bc7ee626159aaed65611041d7a384781a
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2023-09-29 14:27:35 +0200

    feat(server): add swagger
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,24 @@
+import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("I have TODO this")
+    .setDescription("The IHTT API description")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
+
+  Logger.log(`Server running on http://localhost:3000`, "Bootstrap");
+  Logger.log(`Swagger running on http://localhost:3000/api`, "Bootstrap");
 }
 bootstrap();
