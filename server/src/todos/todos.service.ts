1916fc92a3425c8171e8810f66dea2b22527dc36
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2023-09-29 15:56:18 +0200

    feat(server): implements find all of todos service
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- server/src/todos/todos.service.ts
+++ server/src/todos/todos.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+import { Todo } from "./entities/todo.entity";
 
 @Injectable()
 export class TodosService {
+  constructor(
+    @InjectRepository(Todo)
+    private readonly todosRepository: Repository<Todo>,
+  ) {}
+
   findAll() {
-    return `This action returns all todos`;
+    return this.todosRepository.find();
   }
 }
