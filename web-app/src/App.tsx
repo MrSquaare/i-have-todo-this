46b55d7969d7c7e22728d9bee521bea94ec04c2e
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2023-09-29 17:39:18 +0200

    feat(web-app): add todo list component
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,11 +1,27 @@
+import { TodoDTO, TodoState } from "@common/types";
 import { FC } from "react";
+
+import { TodoList } from "./components/todos/list";
+
+const todos: TodoDTO[] = [
+  {
+    id: "1",
+    title: "Todo 1",
+    state: TodoState.TODO,
+  },
+  {
+    id: "2",
+    title: "Todo 2",
+    state: TodoState.DONE,
+  },
+];
 
 export const App: FC = () => {
   return (
-    <div className={"h-screen w-screen bg-neutral-900"}>
-      <h1 className={"text-center text-6xl text-white"} data-testid={"heading"}>
-        I have TODO this
-      </h1>
-    </div>
+    <main className={"flex min-h-screen items-center"}>
+      <div className={"mx-auto w-full max-w-6xl"}>
+        <TodoList todos={todos} />
+      </div>
+    </main>
   );
 };
