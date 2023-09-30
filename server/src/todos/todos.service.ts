0ec9e78721287a942ed5d1cad62264d2a0adc1c3
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2023-09-30 13:54:47 +0200

    feat(server): implement todo service toggle
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- server/src/todos/todos.service.ts
+++ server/src/todos/todos.service.ts
@@ -1,6 +1,9 @@
+import { TodoState } from "@common/types";
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+
+import { NotFoundError } from "../common/errors/not-found";
 
 import { Todo } from "./entities/todo.entity";
 
@@ -16,6 +19,15 @@ export class TodosService {
   }
 
   async toggle(id: Todo["id"]): Promise<Todo> {
-    throw new Error("Not implemented");
+    const todo = await this.todosRepository.findOne({ where: { id } });
+
+    if (!todo) {
+      throw new NotFoundError(`Todo with id ${id} not found`);
+    }
+
+    todo.state =
+      todo.state === TodoState.TODO ? TodoState.DONE : TodoState.TODO;
+
+    return this.todosRepository.save(todo);
   }
 }
