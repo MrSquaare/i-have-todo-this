0129eaf3bb4fa1b4d234d7beafac7bd74992ef65
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2023-10-01 21:26:32 +0200

    feat(server): add class validator decorators on entities
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- server/src/todos/entities/todo.entity.ts
+++ server/src/todos/entities/todo.entity.ts
@@ -1,4 +1,5 @@
 import { TodoDTO, TodoState } from "@common/types";
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { Entity, Column } from "typeorm";
 
 import { BaseEntity } from "../../common/entities/base";
@@ -6,11 +7,15 @@ import { BaseEntity } from "../../common/entities/base";
 @Entity()
 export class Todo extends BaseEntity implements TodoDTO {
   @Column()
+  @IsString()
   title: string;
 
   @Column({ nullable: true })
+  @IsString()
+  @IsOptional()
   description?: string;
 
   @Column({ type: "int", default: TodoState.TODO })
+  @IsEnum(TodoState)
   state: TodoState;
 }
