28d599be505c310f96984a3489d34e41b75a5e32
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2023-10-02 15:55:36 +0200

    feat(server):  add created_at to base entity
    - order by created_at
    
    Signed-off-by: Guillaume Bonnet <mrsquaare@mrsquaare.fr>

--- server/src/todos/todos.factory.ts
+++ server/src/todos/todos.factory.ts
@@ -10,6 +10,7 @@ export class TodosFactory extends Factory<Todo> {
   protected attrs(): Todo {
     return {
       id: faker.string.uuid(),
+      created_at: new Date().toISOString(),
       title: faker.lorem.sentence(),
       description: faker.lorem.paragraph(),
       state: faker.helpers.arrayElement([TodoState.TODO, TodoState.DONE]),
